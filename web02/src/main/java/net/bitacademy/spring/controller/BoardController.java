203d2f32b28e4f69f73469da98ea3d0f6bb7eb9d
SeraJung <tpfk1423@hanmail.net>
2015-06-18 15:10:52 +0900

    service

--- web02/src/main/java/net/bitacademy/spring/controller/BoardController.java
+++ web02/src/main/java/net/bitacademy/spring/controller/BoardController.java
@@ -1,64 +1,63 @@
 package net.bitacademy.spring.controller;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
-import net.bitacademy.spring.dao.BoardDao;
+import net.bitacademy.spring.service.BoardService;
 import net.bitacademy.spring.vo.Board;
+import net.bitacademy.spring.vo.BoardLog;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
   @Autowired
-  BoardDao boardDao;
+  BoardService boardService;
 
   @RequestMapping("/list")
   public String list(Model model)
-      throws Exception {
-    List<Board> boards = boardDao.selectList();
+      throws Exception {    
 
-    model.addAttribute("list", boards);
-    return "/board/list.jsp";
+    model.addAttribute("list", boardService.list());
+    return "board/list";
 
   }
   @RequestMapping(value ="/add",method=RequestMethod.POST )
-  public String add(Board board) throws Exception {
+  public String add(Board board, HttpServletRequest request) throws Exception {
     
     // 파라미터 값을 유니코드로 바꿀때 기본: ISO-8859-1(영어) -->Unicode
     //UTF-8(한글 -->Unicode   
-       
-    boardDao.insert(board);
+    
+    boardService.add(board, request.getRemoteAddr());
     return "redirect:list.do"; 
     
   }
+  
   @RequestMapping(value="/change" , method = RequestMethod.POST)
-  public String change(Board board)  throws Exception {
+  public String change(Board board, HttpServletRequest request)  throws Exception {
     // 파라미터 값을 유니코드로 바꿀때 기본: ISO-8859-1(영어) -->Unicode
     //UTF-8(한글 -->Unicode
-    boardDao.update(board);
+    boardService.change(board, request.getRemoteAddr());
     return "redirect:list.do";
     
   }
   @RequestMapping("remove")
-  public String remove(int no)
+  public String remove(int no, HttpServletRequest request)
       throws Exception {
-    boardDao.delete(no);   
+    boardService.remove(no, request.getRemoteAddr());
     return "redirect:list.do";
    
 
   }
   @RequestMapping("/detail")
-  public String detail(int no, Model model)  throws Exception {
-      Board board = boardDao.selectOne(no);
-      model.addAttribute("board", board);
-      
-      return "/board/detail.jsp";     
+  public String detail(int no, Model model, HttpServletRequest request)  throws Exception {
+      Board board = boardService.get(no, request.getRemoteAddr());
+      model.addAttribute("board", board);      
+      return "board/detail";     
     
 
   }
